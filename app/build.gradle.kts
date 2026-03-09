b32603c20b25d6697d9d69e5a619496c70c32314
kolin <marvelcollin7@gmail.com>
2026-03-09 18:20:16 +0700

    fix: versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.livora"
-        minSdk = 36
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
